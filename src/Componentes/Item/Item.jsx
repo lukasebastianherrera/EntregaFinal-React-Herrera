be236347ccf9ce88dbc2abc3b35559c64a457297
sebastian <lukasebastianherrera2004@gmail.com>
2023-09-18 12:39:53 -0300

    PreEntrega2

--- src/Componentes/Item/Item.jsx
+++ src/Componentes/Item/Item.jsx
@@ -6,7 +6,8 @@ const Item = ({products}) => {
     <div className='card w-25 ' key={products.id}> 
     <div className="card-body">
       <img className='w-100 card-img-top' src={products.img} alt="Mueble" />
-      <p>Precio: {products.price} </p>
+      <p>Producto: {products.name} </p>
+      <p>Precio: ${products.price} </p>
     </div>
     <div className='card-footer'>
       <Link to={`/detalle/${products.id}`}>
